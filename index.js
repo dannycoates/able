0cfc010b51d55507c7001957c929b7c52b243fc2
Danny Coates <dannycoates@gmail.com>
2015-03-02 15:47:40 -0800

    change up index.js based on plugin feedback

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-var Project = require('./project')
-
-module.exports = function (projectDir, gitUrl, cb) {
-  Project.load(
-    projectDir,
-    gitUrl,
-    function (err, project) {
-      if (err) { return cb(err) }
-      return cb(null, project.ab.bind(project))
-    }
-  )
-}
+module.exports = require('./project')
